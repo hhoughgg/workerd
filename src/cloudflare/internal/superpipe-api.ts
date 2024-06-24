ed83aa48593beaa6df0c77acc79ce26d95b77f63
Harry Hough <hhough@cloudflare.com>
2024-06-24 15:23:45 -0500

    added superpipe binding for cloudflare pipelines product

---
+++ src/cloudflare/internal/superpipe-api.ts
@@ -0,0 +1,37 @@
+// Copyright (c) 2024 Cloudflare, Inc.
+// Licensed under the Apache 2.0 license found in the LICENSE file or at:
+// https://opensource.org/licenses/Apache-2.0
+
+import { WorkerEntrypoint } from "./workers"
+
+type PipelineResponse = {
+  status: string
+}
+
+interface Option {}
+
+// rpc target
+declare class BindingInternalEntrypoint extends WorkerEntrypoint {
+  send(data: object[], options?: Option[]): Promise<PipelineResponse>
+}
+
+// wrapped binding for Cloudflare Pipelines product
+// known as SuperPipe internally
+export class Pipeline {
+  private readonly target: BindingInternalEntrypoint
+
+  public constructor(target: BindingInternalEntrypoint) {
+    this.target = target
+  }
+
+  public async send(
+    data: object[],
+    options?: Option[]
+  ): Promise<PipelineResponse> {
+    return this.target.send(data, options)
+  }
+}
+
+export default function makeBinding(env: { target: BindingInternalEntrypoint }): Pipeline {
+  return new Pipeline(env.target)
+}
