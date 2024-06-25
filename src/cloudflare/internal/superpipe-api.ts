c6624522676fc06a8d11cbd030c00a06b03e93c9
Harry Hough <hhough@cloudflare.com>
2024-06-25 11:26:56 -0500

    reverting back to using cloudflare-internal:workers

--- src/cloudflare/internal/superpipe-api.ts
+++ src/cloudflare/internal/superpipe-api.ts
@@ -2,7 +2,7 @@
 // Licensed under the Apache 2.0 license found in the LICENSE file or at:
 // https://opensource.org/licenses/Apache-2.0
 
-import { WorkerEntrypoint } from "cloudflare:workers"
+import { WorkerEntrypoint } from "cloudflare-internal:workers"
 
 type PipelineResponse = {
   status: string
